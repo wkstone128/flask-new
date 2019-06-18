0c16154965795d57e60d2a105fafb85cf6ae3297
王康 <wangkang@wangkangdeMacBook-Pro.local>
2019-06-18 08:50:05 +0800

    添加这册页面

--- code_flask/login.py
+++ code_flask/login.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def index():
     return '我是主页'
 
+# 添加注册页面
+@app.route('/login')
+def login():
+    return '我是注册页面'
 
 if __name__ == "__main__":
     app.run(debug=True,port=8000)
